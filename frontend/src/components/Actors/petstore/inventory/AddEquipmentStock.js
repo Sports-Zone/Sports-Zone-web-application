02a3d5c3e96ffc09f9d564ed98c1b6e850b418c0
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2022-09-14 14:13:34 +0530

    Updated sports zone

--- frontend/src/components/Actors/petstore/inventory/AddEquipmentStock.js
+++ frontend/src/components/Actors/petstore/inventory/AddEquipmentStock.js
@@ -50,41 +50,6 @@ function AddEquipmentStock()
                 <h5 className="main-heading text-center">New Stock Adding Form</h5>
                 <div className="underline mx-auto"></div>
             
-                {/* <div className="form-group">
-                <div className="card">
-                    <div className="card-body bg-c-light">
-                    <div className="row ">
-                <div class="col-md-6">
-                    <label for="option" class="form-label">Medicine</label>
-                    <select class="form-control" id="inputGroupSelect02">
-                        <option selected>Select prescribed medicine</option>
-                        <option value="1">Cotrimaxazole 480 mg</option>
-                        <option value="2">Metronidazole 400 mg</option>
-                        <option value="3">Promethazine 10 mg</option>
-                        <option value="4">Ezo omeprazole 20 mg</option>
-                        <option value="5">Metronidazole 200 mg</option>
-                    </select>
-                    
-                    </div>
-                    <div class="col-md-3">
-                        <div className="form-group">
-                            <label className="mb-1">Quantity</label>
-                            <input type="text" className="form-control" placeholder="Enter quantity of medicine"></input>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div className="form-group">
-                            <label className="mb-1">Available Quantity</label>
-                            <input type="text" className="form-control" ></input>
-                        </div>
-                    </div>
-                </div>
-                    </div>
-                     
-                </div>
-                    
-                    
-                </div> */}
                 <div className="form-field">
         <label htmlFor="service">Equipment List</label>
 
@@ -92,14 +57,7 @@ function AddEquipmentStock()
         {serviceList.map((singleService, index) => (
           <div key={index} className="services">
             <div className="first-division">
-              {/* <input
-                name="service"
-                type="text"
-                id="service"
-                value={singleService.service}
-                onChange={(e) => handleServiceChange(e, index)}
-                required
-              /> */}
+             
 
                 {/* Medicine input parts started here */}
                <div className="form-group">
@@ -108,14 +66,7 @@ function AddEquipmentStock()
                     <div className="row ">
                 <div class="col-md-6">
                     <label for="option" class="form-label">Item</label>
-                    {/* <select class="form-control" id="inputGroupSelect02">
-                        <option selected>Select item to be added</option>
-                        <option value="1">Cotrimaxazole 480 mg</option>
-                        <option value="2">Metronidazole 400 mg</option>
-                        <option value="3">Promethazine 10 mg</option>
-                        <option value="4">Ezo omeprazole 20 mg</option>
-                        <option value="5">Metronidazole 200 mg</option>
-                    </select> */}
+                    
                      <input type="text" className="form-control" placeholder="Enter the title of item"></input>
                     </div>
                     <div class="col-md-3">
@@ -164,15 +115,7 @@ function AddEquipmentStock()
         {/* Medicine input field ended here */}
 
       </div>
-      {/* <div className="output">
-        <h2>Output</h2>
-        {serviceList &&
-          serviceList.map((singleService, index) => (
-            <ul key={index}>
-              {singleService.service && <li>{singleService.service}</li>}
-            </ul>
-          ))}
-      </div> */}
+      
       
 
       {/* Submit and View current availability buttons started here */}
@@ -194,29 +137,6 @@ function AddEquipmentStock()
                 </div>
         
                 
-                {/* <form className="Prescription" autoComplete="off">
-                    <div className="form-field">
-                        <label htmlFor="service">
-                            service
-                        </label>
-                        <div className="services">
-                            <div className="first-devision">
-                                <input name="service" type="text" id="service" required/>
-                                <button type="button" className="add-btn">
-                                    <span>+Add a service</span>
-                                </button>
-                            </div>
-                            <div className="second-devision">
-                                <button type="button" className="remove-btn">
-                                    <span>Remove</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="output">
-                        <h2>Out put</h2>
-                    </div>
-                </form> */}
                 
             </div>
             
